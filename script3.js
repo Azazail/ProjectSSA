51acdcad6ad9bcf851f12415ff6479faf5098464
Arkim Edmond <aedmond@westinghousehs.org>
2017-08-09 15:18:38 +0000

    white page

---
+++ script3.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+$("#p1").click( function () {
+    $("#p1").attr("src");
+    $("#gallery-main").attr("src",$("#p1").attr("src"));
+});
+$("#p2").click( function () {
+    $("#p2").attr("src");
+    $("#gallery-main").attr("src",$("#p2").attr("src"));
+});
+$("#p3").click( function () {
+    $("#p3").attr("src");
+    $("#gallery-main").attr("src",$("#p3").attr("src"));
+});
+$("#p4").click( function () {
+    $("#p4").attr("src");
+    $("#gallery-main").attr("src",$("#p4").attr("src"));
+});
+$("#p5").click( function () {
+    $("#p5").attr("src");
+    $("#gallery-main").attr("src",$("#p5").attr("src"));
+});
+$("#p6").click( function () {
+    $("#p6").attr("src");
+    $("#gallery-main").attr("src",$("#p6").attr("src"));
+});
+$("#p7").click( function () {
+    $("#p7").attr("src");
+    $("#gallery-main").attr("src",$("#p7").attr("src"));
+});
+$("#p8").click( function () {
+    $("#p8").attr("src");
+    $("#gallery-main").attr("src",$("#p8").attr("src"));
+});
+});
